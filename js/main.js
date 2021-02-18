9d59dc8fe3d24ffaed1d23c91710f687486f73c6
hmgreen44 <68399145+hmgreen44@users.noreply.github.com>
2021-02-17 22:34:38 -0500

    dev

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function clock();
+    let currentTime = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    function addZero(component) {
+        return component < 10 ? "0" + component : component;
+      }
+    
+    let period = "AM";{
+        if (hrs == 0) hrs = 12;
+        if (hrs > 12) hrs = hrs - 12;	    
+        
+        period = "PM";
+
+        hrs = hrs < 10 ? "0" + hrs : hrs;
+        mins = mins < 10 ? "0" + mins : mins;
+        secs = secs < 10 ? "0" + secs : secs;
+
+        
+        
+        document.getElementById("clock").innerText = timeString;
+        let Timer = setTimeout(displayCurrentTime, 1000);
+
+        function addZero(component) {
+            return component < 10 ? "0" + component : component;
+          }
+
+      clock();
+      
+}
